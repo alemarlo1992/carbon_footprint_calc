b02f7decc3130ca71f11667707fccee700d08f07
Alejandra Lopez <alemarlo1992@gmail.com>
2020-04-25 11:40:27 -0700

    Updated data model and seeded data to weekly table

--- model.py
+++ model.py
@@ -23,6 +23,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     fname = db.Column(db.String(20), nullable=True, index=True)
     lname = db.Column(db.String(20), nullable=True, index=True)
+    phone = db.Column(db.String(15))
     email = db.Column(db.String(70), nullable=True, unique=True, index=True)
     password_hash = db.Column(db.String(128))
     profile_created_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
@@ -91,122 +92,137 @@ class Rec(db.Model):
                     comment: {self.comment}>"""
 
 
+class Weekly_Rec(db.Model):
+    """Weekly recommendation for users"""
+    __tablename__ = "weekly"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    message = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        """Helpfull representation when printed"""
+        return f"""<id: {self.id}, 
+                    user_id: {self.message},
+                """
+
+
+
 ################################################################################
 #test samples 
-def example_data():
-    """Populate a databse with sample data for testing purposes."""
+# def example_data():
+#     """Populate a databse with sample data for testing purposes."""
 
-    db.create_all()
+#     db.create_all()
 
 
-    #Empty out data from previous runs
-    User.query.delete()
-    Metric.query.delete()
-    Rec.query.delete()
+#     #Empty out data from previous runs
+#     User.query.delete()
+#     Metric.query.delete()
+#     Rec.query.delete()
 
-    #Add sample users, books, and ratings
+#     #Add sample users, books, and ratings
 
-    #sample users
-    user1 = User(user_id=1, email='123@test.com', password_hash='password')
-    user2 = User(user_id=2, email='456@test.com', password_hash='password')
-    user3 = User(user_id=3, email='789@test.com', password_hash='password')
-    user4 = User(user_id=4, email='987@test.com', password_hash='password')
-    user5 = User(user_id=5, email='654@test.com', password_hash='password')
+#     #sample users
+#     user1 = User(user_id=1, email='123@test.com', password_hash='password')
+#     user2 = User(user_id=2, email='456@test.com', password_hash='password')
+#     user3 = User(user_id=3, email='789@test.com', password_hash='password')
+#     user4 = User(user_id=4, email='987@test.com', password_hash='password')
+#     user5 = User(user_id=5, email='654@test.com', password_hash='password')
 
-    user1_metric = Metric(metric_id=1, 
-                            user_id=1, 
-                            trans_metric=10, 
-                            energy_metric=10,
-                            waste_metric=2,
-                            food_metric=2, 
-                            clothing_metric=10, 
-                            created_date="2019-11-27 03:43:47.907932")
+#     user1_metric = Metric(metric_id=1, 
+#                             user_id=1, 
+#                             trans_metric=10, 
+#                             energy_metric=10,
+#                             waste_metric=2,
+#                             food_metric=2, 
+#                             clothing_metric=10, 
+#                             created_date="2019-11-27 03:43:47.907932")
 
-    user2_metric = Metric(metric_id=2, 
-                            user_id=2, 
-                            trans_metric=15, 
-                            energy_metric=10,
-                            waste_metric=5,
-                            food_metric=2, 
-                            clothing_metric=10,
-                            created_date="2019-11-27 03:43:47.907932")
+#     user2_metric = Metric(metric_id=2, 
+#                             user_id=2, 
+#                             trans_metric=15, 
+#                             energy_metric=10,
+#                             waste_metric=5,
+#                             food_metric=2, 
+#                             clothing_metric=10,
+#                             created_date="2019-11-27 03:43:47.907932")
 
-    user3_metric = Metric(metric_id=3, 
-                            user_id=3, 
-                            trans_metric=20, 
-                            energy_metric=10,
-                            waste_metric=2,
-                            food_metric=2, 
-                            clothing_metric=10,
-                            created_date="2019-11-27 03:43:47.907932")
+#     user3_metric = Metric(metric_id=3, 
+#                             user_id=3, 
+#                             trans_metric=20, 
+#                             energy_metric=10,
+#                             waste_metric=2,
+#                             food_metric=2, 
+#                             clothing_metric=10,
+#                             created_date="2019-11-27 03:43:47.907932")
 
-    user4_metric = Metric(metric_id=4, 
-                            user_id=4, 
-                            trans_metric=10, 
-                            energy_metric=20,
-                            waste_metric=2,
-                            food_metric=2, 
-                            clothing_metric=15,
-                            created_date="2019-11-27 03:43:47.907932")
+#     user4_metric = Metric(metric_id=4, 
+#                             user_id=4, 
+#                             trans_metric=10, 
+#                             energy_metric=20,
+#                             waste_metric=2,
+#                             food_metric=2, 
+#                             clothing_metric=15,
+#                             created_date="2019-11-27 03:43:47.907932")
 
-    user5_metric = Metric(metric_id=5, 
-                            user_id=5, 
-                            trans_metric=10, 
-                            energy_metric=30,
-                            waste_metric=2,
-                            food_metric=2, 
-                            clothing_metric=15,
-                            created_date="2019-11-27 03:43:47.907932")
+#     user5_metric = Metric(metric_id=5, 
+#                             user_id=5, 
+#                             trans_metric=10, 
+#                             energy_metric=30,
+#                             waste_metric=2,
+#                             food_metric=2, 
+#                             clothing_metric=15,
+#                             created_date="2019-11-27 03:43:47.907932")
 
-    user1_rec = Rec(rec_id=1, 
-                        user_id=1, 
-                        comment="I eat veggies", 
-                        rec_date="2019-11-27 03:44:48.075786")
+#     user1_rec = Rec(rec_id=1, 
+#                         user_id=1, 
+#                         comment="I eat veggies", 
+#                         rec_date="2019-11-27 03:44:48.075786")
 
-    user2_rec = Rec(rec_id=2, 
-                        user_id=2, 
-                        comment="I drive a tesla", 
-                        rec_date="2019-11-27 03:44:48.075786")
-    user3_rec = Rec(rec_id=3, 
-                        user_id=3, 
-                        comment="I drive a prius", 
-                        rec_date="2019-11-27 03:44:48.075786")
-    user4_rec = Rec(rec_id=4, 
-                        user_id=4, 
-                        comment="I take public trans", 
-                        rec_date="2019-11-27 03:44:48.075786")
-    user5_rec = Rec(rec_id=5, 
-                        user_id=5, 
-                        comment="I'm vegan", 
-                        rec_date="2019-11-27 03:44:48.075786")
+#     user2_rec = Rec(rec_id=2, 
+#                         user_id=2, 
+#                         comment="I drive a tesla", 
+#                         rec_date="2019-11-27 03:44:48.075786")
+#     user3_rec = Rec(rec_id=3, 
+#                         user_id=3, 
+#                         comment="I drive a prius", 
+#                         rec_date="2019-11-27 03:44:48.075786")
+#     user4_rec = Rec(rec_id=4, 
+#                         user_id=4, 
+#                         comment="I take public trans", 
+#                         rec_date="2019-11-27 03:44:48.075786")
+#     user5_rec = Rec(rec_id=5, 
+#                         user_id=5, 
+#                         comment="I'm vegan", 
+#                         rec_date="2019-11-27 03:44:48.075786")
 
-     #Add all to session and commit
-    db.session.add_all([user1, user2, user3, user4, user5, user1_metric, user2_metric,
-                        user3_metric, user4_metric, user5_metric, user1_rec, user2_rec, 
-                        user3_rec, user4_rec, user5_rec])
+#      #Add all to session and commit
+#     db.session.add_all([user1, user2, user3, user4, user5, user1_metric, user2_metric,
+#                         user3_metric, user4_metric, user5_metric, user1_rec, user2_rec, 
+#                         user3_rec, user4_rec, user5_rec])
 
-    db.session.commit()
+#     db.session.commit()
 
 
 ################################################################################
-def connect_to_db(app, db_uri='postgresql:///carboncalculator'):
-    """Connect the database to our Flask app."""
 
-    # Configure to use our PstgreSQL database
-    # Configure to use our PostgreSQL database
+def connect_to_db(app, db_uri = 'postgresql:///carboncalculator'):
+    '''Connect the database to app'''
+
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+
     db.app = app
     db.init_app(app)
 
-if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
-    # you in a state of being able to work with the database directly.
+
+if __name__ == '__main__':
+
     from server import app
-    # init_app()
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-
+    connect_to_db(app)
+    #for using interactively
+    print('Connected to DB.')
 
 
 
