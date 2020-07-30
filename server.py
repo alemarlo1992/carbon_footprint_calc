d3911cb31d3a0a2570bd7a0a1ca3b981d2d47873
Alejandra Lopez <alemarlo1992@gmail.com>
2020-07-30 13:54:43 -0700

    Updated HTML templates

--- server.py
+++ server.py
@@ -372,6 +372,7 @@ if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
     app.debug = False
+
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
